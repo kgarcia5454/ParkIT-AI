f65101c63a802439555c176f2b85e33606b87f2a
Noel0831 <newilso6@asu.edu>
2021-10-02 16:23:26 -0500

    Update Main.java

--- ParkIt AI/src/main/java/Main.java
+++ ParkIt AI/src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
 	public static void main(String[] args) {
 		DynamoDbClient.builder().region(Region.US_EAST_1).build();
-		System.out.println("Hello World");
+		System.out.println("HWorld");
 
 	}
 
